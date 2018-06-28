d933f59be561fa9997c6e3157c607be9ae5c38e0
Aavash <aavash.khatri@aayulogic.com>
2018-06-28 17:16:27 +0545

    Crud and permissions for models

--- article/urls.py
+++ article/urls.py
@@ -9,9 +9,13 @@ router = DefaultRouter()
 # router.register(r'snippets', views.SnippetViewSet)
 
 urlpatterns = [
-    url(r'^all_articles/$', views.ArticleListAPIView),
-    url(r'^published_articles/$', views.ArticleListPublishedAPIView),
-    url(r'^article_details/(?P<pk>[0-9]+)/$', views.APIView),
+    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^published/$', views.ArticleListPublishedAPIView.as_view(), name='published_articles'),
+    url(r'^create/$', views.ArticleCreateAPIView.as_view(), name='create_article'),
+    url(r'^all/$', views.ArticleListAPIView.as_view(), name='all_articles'),
+    url(r'^details/(?P<pk>[0-9]+)/$', views.ArticleDetailAPIView.as_view(), name='article_details'),
+    url(r'^update/(?P<pk>[0-9]+)/$', views.ArticleUpdateAPIView.as_view(), name='article_update'),
+    url(r'^delete/(?P<pk>[0-9]+)/$', views.ArticleDeleteAPIView.as_view(), name='article_delete'),
 ]
 
 
--- article/views.py
+++ article/views.py
@@ -1,63 +1,150 @@
-from django.http import Http404
-
+from article.permissions import IsOwnerOrReadOnly
 from models import Article, Like, Comment
+from rest_framework import mixins
+
 from serializers import ArticleSerializer, UserSerializer, LikeSerializer, CommentSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
+from rest_framework import permissions
 
 
-class ArticleListPublishedAPIView(APIView):
-    """
-    List all the published api
-    """
-    def get(self, request, format=None):
-        articles = Article.objects.filter(published= True)
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
+class ArticleListPublishedAPIView(generics.ListAPIView):
+    model = Article
+    queryset = Article.objects.filter(published=True)
+    serializer_class = ArticleSerializer
 
 
-class ArticleListAPIView(APIView):
-    """"
-    List all the articles or create a new article
-    """
-    def get(self, request, format=None):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ArticleListAPIView(generics.ListAPIView):
+    model = Article
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
 
-class ArticleDetailAPIView(APIView):
-    """
-    Retrieve, update or delete a article instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Article.objects.get(pk=pk)
-        except Article.DoesNotExist:
-            raise Http404
+class ArticleDetailAPIView(generics.RetrieveAPIView):
+    model = Article
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
-    def get(self, request, pk, format=None):
-        article = self.get_object(pk)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        article = self.get_object(pk)
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ArticleCreateAPIView(generics.CreateAPIView):
+    model = Article
+    queryset = Article.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = ArticleSerializer
 
-    def delete(self, request, pk, format=None):
-        article = self.get_object(pk)
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ArticleUpdateAPIView(generics.UpdateAPIView):
+    model = Article
+    queryset = Article.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    serializer_class = ArticleSerializer
+
+
+class ArticleDeleteAPIView(generics.DestroyAPIView):
+    model = Article
+    queryset = Article.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    serializer_class = ArticleSerializer
+
+
+class LikeCreateAPIView(generics.CreateAPIView):
+    model = Like
+    queryset = Like.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = LikeSerializer
+
+
+class LikeDeleteAPIView(generics.DestroyAPIView):
+    model = Like
+    queryset = Like.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    serializer_class = LikeSerializer
+
+
+# class CommentListAPIView(generics.ListAPIView):
+#     permission_classes = (permissions.IsAuthenticated,)
+#     serializer_class = CommentSerializer
+
+
+class CommentCreateAPIView(generics.CreateAPIView):
+    model = Comment
+    queryset = Comment.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = CommentSerializer
+
+
+class CommentUpdateAPIView(generics.UpdateAPIView):
+    model = Comment
+    queryset = Comment.objects.all()
+    permission_classes = (IsOwnerOrReadOnly,)
+    serializer_class = CommentSerializer
+
+
+class CommentDeleteAPIView(generics.DestroyAPIView):
+    model = Comment
+    queryset = Comment.objects.all()
+    permission_classes = (permissions.IsAdminUser, IsOwnerOrReadOnly,)
+    serializer_class = CommentSerializer
+
+# from django.http import Http404
+#
+# from models import Article, Like, Comment
+# from serializers import ArticleSerializer, UserSerializer, LikeSerializer, CommentSerializer
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+#
+#
+# class ArticleListPublishedAPIView(APIView):
+#     """
+#     List all the published api
+#     """
+#     def get(self, request, format=None):
+#         articles = Article.objects.filter(published= True)
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
+#
+#
+# class ArticleListAPIView(APIView):
+#     """"
+#     List all the articles or create a new article
+#     """
+#     def get(self, request, format=None):
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class ArticleDetailAPIView(APIView):
+#     """
+#     Retrieve, update or delete a article instance.
+#     """
+#     def get_object(self, pk):
+#         try:
+#             return Article.objects.get(pk=pk)
+#         except Article.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk, format=None):
+#         article = self.get_object(pk)
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         article = self.get_object(pk)
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         article = self.get_object(pk)
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
